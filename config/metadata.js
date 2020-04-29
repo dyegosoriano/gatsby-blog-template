93e4b203ee8a02e0a884dfe79377e5daf51d721e
Dyego Soriano <dyegodf2@gmail.com>
2020-04-29 14:20:15 -0300

    update plugin

---
+++ config/metadata.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: `Clinica Life`,
+  author: `Dyego Soriano`,
+  siteUrl: `https://dyegosoriano.github.io/`,
+  description: `Inovação em saúde e bem estar`,
+  social: [
+    {
+      name: "github",
+      url: "https://github.com/dyegosoriano",
+    },
+    {
+      name: "instagram",
+      url: "https://www.instagram.com/dyego_df2",
+    },
+  ],
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,7 @@
+const siteMetadata = require("./config/metadata")
+
 module.exports = {
-  siteMetadata: {
-    title: "My personal blog",
-    titleTemplate: "%s · Blog",
-    description: "Nostrud est duis proident ut dolore ipsum tempor Lorem.",
-    author: "danilowoz",
-    twitterUsername: "@danilowoz",
-  },
+  siteMetadata,
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
@@ -20,18 +16,30 @@ module.exports = {
         plugins: [`gatsby-remark-images`],
       },
     },
+    {
+      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      options: {
+        fonts: [
+          {
+            family: `Open Sans`,
+            variants: [`300`, `500`, `700`],
+          },
+        ],
+      },
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: siteMetadata.title,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#212121`,
+        theme_color: `#76ff03`,
         display: `minimal-ui`,
         // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
