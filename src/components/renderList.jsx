5194bd1488b4424bff7536a8978ff7df8710b94a
Dyego Soriano <dyegodf2@gmail.com>
2020-04-29 18:09:41 -0300

    update renderList

--- src/components/renderList.jsx
+++ src/components/renderList.jsx
@@ -31,11 +31,14 @@ const renderList = ({ node }) => {
           <Time>{formatDate(node.frontmatter.date)}</Time>
         </BoxCenter>
 
-        {node.frontmatter.category.map((cat, index, arr) => (
-          <ConcatWords arrCount={arr.length} index={index} key={cat}>
-            <Link to={`/blog/category/${kebabCase(cat)}`}>{cat}</Link>
-          </ConcatWords>
-        ))}
+        <BoxCenter>
+          <Span>Tags:</Span>
+          {node.frontmatter.category.map((cat, index, arr) => (
+            <ConcatWords arrCount={arr.length} index={index} key={cat}>
+              <Tag to={`/blog/category/${kebabCase(cat)}`}>{cat}</Tag>
+            </ConcatWords>
+          ))}
+        </BoxCenter>
       </BoxRight>
     </Article>
   )
@@ -46,30 +49,32 @@ export default renderList
 const Article = styled.article`
   display: grid;
   grid-template-columns: 400px 1fr;
-  margin-bottom: 50px;
+  margin-bottom: 100px;
   width: 100%;
-`
-const Image = styled.img`
-  width: 100%;
+  height: 250px;
 `
 const Banner = styled(Link)`
   grid-column: 1;
+`
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
 `
 const BoxRight = styled.div`
   grid-column: 2;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
 `
 const Title = styled.h1`
-  font-size: 44px;
+  font-weight:bold;
+  font-size: 2rem;
   text-align: center;
 `
 const BoxCenter = styled.div`
   display: flex;
   justify-content: center;
-  padding: 10px 0;
+  margin-bottom:40px;
 `
 const Author = styled.h2`
   font-size: 1rem;
@@ -79,4 +84,20 @@ const Time = styled.time`
   font-size: 1rem;
   margin-left: 20px;
   color: #919191;
+`
+const Span = styled.samp`
+  font-size:1rem;
+  font-weight:900;
+`
+const Tag = styled(Link)`
+  font-size:1rem;
+  background: #000;
+  color: #fff;
+  border-radius:3px;
+  padding: 2px 5px;
+  margin: 0px 5px;
+  &:hover {
+    color: #fff;
+    background: #919191;
+  }
 `
