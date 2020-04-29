27c21308b849d986d59bd96ee444d17db5419e8f
Dyego Soriano <dyegodf2@gmail.com>
2020-04-29 18:10:35 -0300

    update concatWords

--- src/utils/ConcatWords.js
+++ src/utils/ConcatWords.js
@@ -1,22 +1,5 @@
 import * as React from "react"
 
 export default ({ arrCount, index, children }) => {
-  if (arrCount === 1) {
-    return <>{children}</>
-  }
-
-  if (index + 2 < arrCount) {
-    return <>{children}, </>
-  }
-
-  // if (index + 1 === arrCount) {
-  //   return (
-  //     <>
-  //       {" "}
-  //       {"&"} {children}
-  //     </>
-  //   )
-  // }
-
   return <>{children}</>
 }
