e86b500af68ada660ce2d7bbd0e25b47ae8ce0c2
Dyego Soriano <dyegodf2@gmail.com>
2020-04-29 17:45:24 -0300

    update featured

--- src/components/featured.jsx
+++ src/components/featured.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import kebabCase from "lodash.kebabcase"
+import styled from "styled-components"
 import { graphql, Link, useStaticQuery } from "gatsby"
 
 import ConcatWords from "../utils/ConcatWords"
@@ -10,38 +11,37 @@ const BlogFeatured = () => {
   const imageSource = markdownRemark.frontmatter.image.childImageSharp.fluid.src
 
   return (
-    <>
-      <div>
-        <Link to={markdownRemark.fields.slug}>
-          <img src={imageSource} alt={markdownRemark.frontmatter.title} />
-        </Link>
-      </div>
+    <Features>
+      <Link to={markdownRemark.fields.slug}>
+        <Title>{markdownRemark.frontmatter.title}</Title>
+      </Link>
 
-      <div>
-        <p>
-          By{" "}
+      <BoxCenter>
+        <Author>
+          autor{" "}
           <Link
             to={`/blog/author/${kebabCase(markdownRemark.frontmatter.author)}`}
           >
             {markdownRemark.frontmatter.author}
           </Link>
-        </p>
+        </Author>
 
-        <Link to={markdownRemark.fields.slug}>
-          <h2>{markdownRemark.frontmatter.title}</h2>
-        </Link>
+        <Time>{formatDate(markdownRemark.frontmatter.date)}</Time>
+      </BoxCenter>
 
-        <p>{formatDate(markdownRemark.frontmatter.date)}</p>
+      <Link to={markdownRemark.fields.slug}>
+        <Banner src={imageSource} alt={markdownRemark.frontmatter.title} />
+      </Link>
 
-        <p>
-          {markdownRemark.frontmatter.category.map((cat, index, arr) => (
-            <ConcatWords arrCount={arr.length} index={index} key={cat}>
-              <Link to={`/blog/category/${kebabCase(cat)}`}>{cat}</Link>
-            </ConcatWords>
-          ))}
-        </p>
-      </div>
-    </>
+      <BoxCenter>
+        <Span>Tags:</Span>
+        {markdownRemark.frontmatter.category.map((cat, index, arr) => (
+          <ConcatWords arrCount={arr.length} index={index} key={cat}>
+            <Tag to={`/blog/category/${kebabCase(cat)}`}>{cat}</Tag>
+          </ConcatWords>
+        ))}
+      </BoxCenter>
+    </Features>
   )
 }
 
@@ -49,24 +49,66 @@ export default BlogFeatured
 
 const query = graphql`
   query BlogFeatured {
-    markdownRemark(frontmatter: {featured: {eq: true}}) {
-    html
-    fields {
-      slug
-    }
-    frontmatter {
-      title
-      author
-      category
-      image {
-        childImageSharp {
-          fluid {
-            src
+    markdownRemark(frontmatter: { featured: { eq: true } }) {
+      html
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+        author
+        category
+        image {
+          childImageSharp {
+            fluid(maxHeight: 400, maxWidth: 960) {
+              src
+            }
           }
         }
+        date(formatString: "DD MM YYYY")
       }
-      date(formatString: "DD MM YYYY")
     }
   }
+`
+
+const Features = styled.article`
+  margin-top: 50px;
+  margin-bottom: 150px;
+`
+const Title = styled.h1`
+  font-size:3rem;
+  text-align:center;
+`
+const Banner = styled.img`
+  width: 100%;
+`
+const BoxCenter = styled.div`
+  display: flex;
+  margin-bottom:25px;
+  justify-content: center;
+`
+const Author = styled.h2`
+  font-size: 1rem;
+  color: #919191;
+`
+const Time = styled.time`
+  font-size: 1rem;
+  margin-left: 20px;
+  color: #919191;
+`
+const Span = styled.samp`
+  font-size:1rem;
+  font-weight:900;
+`
+const Tag = styled(Link)`
+  font-size:1rem;
+  background: #000;
+  color: #fff;
+  border-radius:3px;
+  padding: 2px 5px;
+  margin: 0px 5px;
+  &:hover {
+    color: #fff;
+    background: #919191;
   }
 `
